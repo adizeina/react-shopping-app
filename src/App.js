956983770dd2316c784ffb773a256193f8656898
*Zeina* <zeina.adi@mail.utoronto.ca>
2021-06-28 12:31:46 -0400

    Adding redux to the filter (backend)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import data from "./data.json";
+// import data from "./data.json";  --> no longer need because the data comes from the back end 
 import Products from "./components/Products";
 import Filter from './components/Filter'
 import Cart from './components/Cart'
@@ -11,36 +11,31 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      products: data.products,
+      //products: data.products,     comes from backend now
       // to store cart items so it doesn't disappear on refresh --> if the cart is not empty then parse, else --> empty array
       cartItems: localStorage.getItem("cartItems")? JSON.parse(localStorage.getItem("cartItems")):[], 
-      size:"",
-      sort:"",
+      // size:"",   no longer needed because we use Redux store now
+      // sort:"",   no longer needed because we use Redux store now
     };
   }
-
-  sortProducts = (event) => {
-    const sort = event.target.value;
-    console.log(event.target.value);
-    this.setState((state) => ({
-      sort: sort, 
-      products: this.state.products
-      .slice()
-      .sort((a,b) =>
-        sort === "lowest"
-          ? a.price > b.price
-            ? 1
-            :-1
-        : sort === "highest"
-        ? a.price < b.price
-          ? 1
-          :-1
-        : a._id < b._id
-          ? 1
-          :-1
-      ),
-    }));
-  };
+/////// NO longer needed due to Redux 
+  // sortProducts = (event) => {
+   //  const sort = event.target.value;
+   //  console.log(event.target.value);
+   //  this.setState((state) => ({
+   //  sort: sort, 
+   //  products: this.state.products
+   //   .slice()
+   //   .sort((a,b) =>
+    //    sort === "lowest"
+   //       ? a.price > b.price
+    //        ? 1 :-1
+     //   : sort === "highest"
+     //   ? a.price < b.price
+     //     ? 1 :-1
+     //   : a._id < b._id
+     //     ? 1:-1
+    //  ), }));   };
 
   addToCart = (product) =>{
     const cartItems = this.state.cartItems.slice();
@@ -58,19 +53,21 @@ class App extends React.Component {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));   //to stop cartItems from disappearing on refresh
   };
 
-  filterProducts = (event) => {
-    console.log(event.target.value);
-    if (event.target.value ===""){
-      this.setState({size: event.target.value, product: data.products});
-    } else {
-    this.setState({
-      size: event.target.value, 
-      products: data.products.filter(
-        (product) => product.availableSizes.indexOf(event.target.value) >=0 
-      ), 
-  });
-};
-};
+
+////// NO longer needed due to Redux 
+//filterProducts = (event) => {
+    //console.log(event.target.value);
+   // if (event.target.value ===""){
+    //  this.setState({size: event.target.value, product: data.products});
+   // } else {
+   // this.setState({
+    //  size: event.target.value, 
+    //  products: data.products.filter(
+    //    (product) => product.availableSizes.indexOf(event.target.value) >=0 
+  //    ), 
+ // });
+//};
+//};
   removeFromCart = (product) => {
     const cartItems = this.state.cartItems.slice();
     this.setState({cartItems: cartItems.filter((x) => x._id !== product._id), 
@@ -94,12 +91,8 @@ class App extends React.Component {
       <main>
         <div className="content">
           <div className="main">
-            <Filter count = {this.state.products.length}   //num of products
-            size={this.state.size}
-            sort = {this.state.sort}
-            filterProducts={this.filterProducts}
-            sortProducts={this.sortProducts}></Filter>
-            <Products products = {this.state.products} addToCart={this.addToCart}></Products>
+            <Filter></Filter>
+            <Products addToCart={this.addToCart}></Products>
           </div>
 
           <div className="sidebar">
