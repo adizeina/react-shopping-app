8a71a5f98e072b058c90f3265236314efea3dc6b
*Zeina* <zeina.adi@mail.utoronto.ca>
2021-07-01 10:15:47 -0400

    creating an order modal

--- src/store.js
+++ src/store.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import {productsReducer} from './reducers/productReducers';
 import thunk from 'redux-thunk';
-import { cartReducer } from "./reducers/cartReducer";
+import { cartReducer } from "./reducers/cartReducers";
+import { orderReducer } from "./reducers/orderReducers";
 
 
 const initialState = {};
@@ -9,6 +10,7 @@ const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(combineReducers({
     products: productsReducer,
     cart: cartReducer,
+    order: orderReducer,
 
     }), 
     initialState, 
